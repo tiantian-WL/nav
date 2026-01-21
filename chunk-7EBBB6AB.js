1a57b9f7b937f35f8e848e3783110d7782738707
tiantian-WL <850725039@qq.com>
2026-01-21 14:46:07 +0000

    Deploying to gh-pages from @ tiantian-WL/nav@6f449aa5ad0c625cb4a40b7703951eb696e4fb7f 🚀

--- chunk-7EBBB6AB.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./chunk-K5LUV7MB.js";import{Ab as e,sb as r}from"./chunk-62HNKKVV.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
